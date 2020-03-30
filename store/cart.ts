82ae0b6435375c6bbdd01b0de934017e5af51fe0
Jakob Nanneson <jakob.nanneson@coop.se>
2020-03-30 13:27:29 +0200

    use try and await when using axios

--- store/cart.ts
+++ store/cart.ts
@@ -2,6 +2,16 @@ export const actions = {
   async fetchCart({ rootState, commit }) {
     const { config } = rootState
 
+    try {
+      const { data } = await this['$axios'].get(
+        `https://www.coop.se/ws/v2/coop/users/${config.user}/carts/${config.cartguid}?fields=DEFAULT`
+      )
+      commit('setCart', data)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+
     await this['$axios']
       .get(
         `https://www.coop.se/ws/v2/coop/users/${config.user}/carts/${config.cartguid}?fields=DEFAULT`
