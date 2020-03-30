98ea95c99fa83ef88857a06dd1bad137040e81ca
Jakob Nanneson <jakob.nanneson@coop.se>
2020-03-30 13:11:35 +0200

    removed duplicated bearer auth

--- plugins/config.js
+++ plugins/config.js
@@ -1,6 +1,7 @@
 export default function({ store }) {
   if (process.env.NODE_ENV !== 'production' && !('ACC' in global)) {
-    // tip: console copy(window.ACC.config)
+    // tip: console
+    // copy(window.ACC.config)
     store.dispatch('config/init', {
       contextPath: '/handla',
       cartguid: '145bda80-f874-4671-ab0b-c2be71355140',
@@ -11,8 +12,8 @@ export default function({ store }) {
       rootPath: '/handla/_ui/responsive',
       CSRFToken: '',
       authToken:
-        'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OEIzMjQwOTExQkE4QkYxRUZDNzI2NDI3MTFGQ0U3RkM0RUNEMkMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJsNHN5UUpFYnFMOGVfSEprSnhIODVfeE96U3cifQ.eyJuYmYiOjE1ODUzMDU5ODgsImV4cCI6MTU4NTMwOTU4OCwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5jb29wLnNlIiwiYXVkIjpbImh0dHBzOi8vbG9naW4uY29vcC5zZS9yZXNvdXJjZXMiLCJoeWJyaXNfYXBpIl0sImNsaWVudF9pZCI6Imh5YnJpc19jbXNfcHJvZCIsInN1YiI6ImExNDg2NDllLTIzNWEtNDE1Ny04ZGY4LTViMmFhNDI0ZWE3ZCIsImF1dGhfdGltZSI6MTU4NTEzMTg2MSwiaWRwIjoibG9jYWwiLCJlbm92YUlkIjoiMjA5MjMxMjI0IiwiaXNDb21wYW55IjoiRmFsc2UiLCJsYXN0TG9naW5EYXRlIjoiMjAyMC0wMy0yNSAxMToyNDoyMSIsImlkMzYwIjoiM2I0YmJiNTQtM2IxYS00MWExLWJmZDMtMzdiODFlZjE4ODNiIiwiZW1haWwiOiJqYWtvYi5uYW5uZXNvbkBjb29wLnNlIiwic2NvcGUiOlsiaHlicmlzLnByb2ZpbGUiLCJvcGVuaWQiLCJoeWJyaXNfYXBpIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdfQ.Fs9xrXTSmsKiUn2xOudnzkl4mEUb_GOqluHXzOaVNLQ6t9-h4rNAqjNK1hFfw03f7BHIlN1wMuJEtSdCtp12Rf-UvzugdaHR2w1SWItUcqNvBg-FV_JBaPsCMb1lEcVK9dVkcktl9bNV-Ph4IKgG412OP4dYE7T6NGb6NBWjlJAcebOoFom4Gwf-TzBGmg8ZD3BGVs8oZDXS56oeJs6xCQkZy8XJ8Z1_JExe8Yr_3t9T0Ti1kxeEVVZfNkEVrwRam64S9AgiSik2jTZfJxja4FPAqW3OfTx_NYF5y7eN0RCTZEwUDhFeZUdW_RSuXNAgBeoXSIMqCReggI8M1wEkVg',
-      rrSessionId: 's3946166023056',
+        'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OEIzMjQwOTExQkE4QkYxRUZDNzI2NDI3MTFGQ0U3RkM0RUNEMkMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJsNHN5UUpFYnFMOGVfSEprSnhIODVfeE96U3cifQ.eyJuYmYiOjE1ODU1NjQyODcsImV4cCI6MTU4NTU2Nzg4NywiaXNzIjoiaHR0cHM6Ly9sb2dpbi5jb29wLnNlIiwiYXVkIjpbImh0dHBzOi8vbG9naW4uY29vcC5zZS9yZXNvdXJjZXMiLCJoeWJyaXNfYXBpIl0sImNsaWVudF9pZCI6Imh5YnJpc19jbXNfcHJvZCIsInN1YiI6ImExNDg2NDllLTIzNWEtNDE1Ny04ZGY4LTViMmFhNDI0ZWE3ZCIsImF1dGhfdGltZSI6MTU4NTEzMTg2MSwiaWRwIjoibG9jYWwiLCJlbm92YUlkIjoiMjA5MjMxMjI0IiwiaXNDb21wYW55IjoiRmFsc2UiLCJsYXN0TG9naW5EYXRlIjoiMjAyMC0wMy0yNSAxMToyNDoyMSIsImlkMzYwIjoiM2I0YmJiNTQtM2IxYS00MWExLWJmZDMtMzdiODFlZjE4ODNiIiwiZW1haWwiOiJqYWtvYi5uYW5uZXNvbkBjb29wLnNlIiwic2NvcGUiOlsiaHlicmlzLnByb2ZpbGUiLCJvcGVuaWQiLCJoeWJyaXNfYXBpIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdfQ.XfR-VBa1bo6WCBwRLGevzbbGWLbV_qaBk2T2aPAYrvAN02E_3hSRYbYJx3LcTm1paBEQAEEhbfg2zM-TMWK8dzrXBXAHjTvTAn5cBvY27kVzg0OZAihrrEeXNerkprI3fa391rka2oSQxdzsTD-pq0CdkUbbQpD9wM3RgVz3YysmUHh1p4qYk9VnW23OeQau_KURLuRpYN6rKBEiWwk2ly5oW3yrhdMX46ORXr4laXaqVdLnqCIbHup4sVmeOVtQepcaXE95uKS5vBboIe-qDTKWHEh22GJUAd0QrRyUMcNELeFDUvQRv6PInDL6QExSdbsGZdxgVTr_ze4FrBbHTw',
+      rrSessionId: 's3950298903872',
       rrPersonalizeEndpoint:
         '/users/a148649e-235a-4157-8df8-5b2aa424ea7d/personalize/personalize?placements=home_page.TopContent',
       rrProductsEndpoint:
@@ -25,19 +26,20 @@ export default function({ store }) {
       numberOfOrders: '0',
       hasSavedCarts: false,
       medmeraId: '',
-      sessionPostCode: '',
+      sessionPostCode: '11647',
       googleApiKey: '',
       googleApiVersion: '3.7',
       swishPayment: '',
-      pointOfService: '252711',
+      pointOfService: '',
       b2bUser: false,
       externalAddToCartRecipe: '',
       orderConfirmationPlacementId: 'purchase_complete_page',
       loginUrl: '/handla/openid_connect_login',
       authenticationStatusUrl: '/handla/authentication/status',
+      postCode: '11647',
       deliveryMode: {
-        code: 'pickup_drive_through',
-        name: 'Hämta med bil'
+        code: 'homedelivery',
+        name: 'Hemleverans'
       }
     })
   } else {
--- store/cart.ts
+++ store/cart.ts
@@ -2,19 +2,9 @@ export const actions = {
   async fetchCart({ rootState, commit }) {
     const { config } = rootState
 
-    let axiosConfig = {}
-    if (config.user !== 'anonymous') {
-      axiosConfig = {
-        headers: {
-          Authorization: `Bearer ${config.authToken}`
-        }
-      }
-    }
-
     await this['$axios']
       .get(
-        `https://www.coop.se/ws/v2/coop/users/${config.user}/carts/${config.cartguid}?fields=DEFAULT`,
-        axiosConfig
+        `https://www.coop.se/ws/v2/coop/users/${config.user}/carts/${config.cartguid}?fields=DEFAULT`
       )
       .then((response) => {
         commit('setCart', response.data)
