85066c05b7f254022069232bae2a8fb6d1aa4d04
Jakob Nanneson <jakob.nanneson@coop.se>
2020-04-01 15:13:55 +0200

    remove duplicated cart fetch

--- store/cart.ts
+++ store/cart.ts
@@ -13,18 +13,6 @@ export const actions = {
       // eslint-disable-next-line no-console
       console.error(error)
     }
-
-    await this['$axios']
-      .get(
-        `https://www.coop.se/ws/v2/coop/users/${config.user}/carts/${config.cartguid}?fields=DEFAULT`
-      )
-      .then((response) => {
-        commit('setCart', response.data)
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error(error)
-      })
   }
 }
 
