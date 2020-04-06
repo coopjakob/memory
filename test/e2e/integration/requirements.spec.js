a587cd429e6f9bb1dee4449dcf6dbe149e58c57e
Jakob Nanneson <jakob.nanneson@coop.se>
2020-04-06 19:33:34 +0200

    add Percy and adjusted Cypress test

--- test/e2e/integration/requirements.spec.js
+++ test/e2e/integration/requirements.spec.js
@@ -7,10 +7,12 @@ context('extra cards', () => {
 
   it('info card visible', () => {
     cy.get('.info-card').should('have.length.greaterThan', 0)
+    cy.percySnapshot()
   }),
 
   it('ad card visible', () => {
     cy.get('.ad-card').should('have.length.greaterThan', 0)
+    cy.percySnapshot()
   }),
 
   it('ad card at first position', () => {
@@ -35,33 +37,32 @@ context('show 3 rows of products', () => {
     cy.viewport(375, 1050)
     cy.visit('/')
     cy.get('.card').should('have.length', 6)
+    cy.percySnapshot('375', {widths: [375]})
   }),
 
   it('tablet show 12 cards, 768 screen', () => {
     cy.viewport(728, 1050)
     cy.visit('/')
     cy.get('.card').should('have.length', 12)
+    cy.percySnapshot('728', {widths: [728]})
   }),
 
   it('1024 show 18 on 1024', () => {
     cy.viewport(944, 1050)
-    cy.get('.card')
-      .its('length')
-      cy.get('.card').should('have.length', 18)
+    cy.get('.card').should('have.length', 18)
+    cy.percySnapshot('944', {widths: [944]})
   }),
 
   it('1145 show 21', () => {
     cy.viewport(1145, 1050)
-    cy.get('.card')
-      .its('length')
-      cy.get('.card').should('have.length', 21)
+    cy.get('.card').should('have.length', 21)
+    cy.percySnapshot('1145', {widths: [1145]})
   }),
 
   it('big screen show 24', () => {
     cy.viewport(1320, 1050)
-    cy.get('.card')
-      .its('length')
-      cy.get('.card').should('have.length', 24)
+    cy.get('.card').should('have.length', 24)
+    cy.percySnapshot('1320', {widths: [1320]})
   })
 
 }),
@@ -73,6 +74,7 @@ context('on mobile', () => {
     cy.visit('/')
     cy.get('.show-more button').click()
     cy.get('.card').should('have.length.greaterThan', 6)
+    cy.percySnapshot('show more', {widths: [375]})
   })
 
 })
