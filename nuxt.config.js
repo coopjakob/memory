781fe8c583ed8c949e62e0c28fc8e681a66dbc86
Jakob Nanneson <jakob.nanneson@coop.se>
2020-04-07 10:56:49 +0200

    meta only on dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,21 @@
 const webpack = require('webpack')
 
+let meta = []
+
+if (process.env.NODE_ENV === 'development') {
+  meta = [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ]
+}
+
 export default {
   mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    ]
+    meta: meta
   },
   /*
    ** Customize the progress-bar color
