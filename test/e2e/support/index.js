bc67515c59f57f23684b4516bb227bca567446bc
Max Malm <benjick@dumfan.net>
2020-04-09 12:25:12 +0200

    Set up fixtures

--- test/e2e/support/index.js
+++ test/e2e/support/index.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+beforeEach(() => {
+  cy.server()
+  cy.route('**home_page.2020_start_full*', 'fixture:full.json').as('getFull')
+  cy.route('**home_page.2020_start_few*', 'fixture:few.json').as('getFew')
+})
